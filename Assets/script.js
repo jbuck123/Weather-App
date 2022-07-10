32a37baab93dc7dd690ff7b71fe23e60a4f95bd3
JamesB <James.buchmann@gmail.com>
2022-07-10 13:32:37 -0500

    updated css and html

--- Assets/script.js
+++ Assets/script.js
@@ -1,1 +1,25 @@
-console.log("online");
+var inputValue = document.querySelector(".user-input");
+var btn = doucmet.querySelector(".btn");
+
+// const weatherdata = () => {
+//     fetch("https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=bf593cc145bddaaa5aef0a13fbd078bf")
+//     .then((data) => data.json())
+//     .then((data) => console.log(data));
+
+
+
+// };
+
+// weatherdata();
+btn.addEventListener('click', function(){
+    fetch("https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=bf593cc145bddaaa5aef0a13fbd078bf")
+    .then(Response => Response.json())
+    .then(data => console.log(data))
+    
+    .catch(err => alert("wrong city name !")) 
+})
+fetch("https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=bf593cc145bddaaa5aef0a13fbd078bf")
+.then(Response => Response.json())
+.then(data => console.log(data))
+
+.catch(err => alert("wrong city name !"))
